49b9ce08b47a42ac578d2b0b24bcd2d9067624da
shivam-kumar6 <shivam.kumar6@oyorooms.com>
2020-09-24 12:55:37 +0530

    minor changes

--- src/main/java/com/project/in/teams/Controllers/UsersController.java
+++ src/main/java/com/project/in/teams/Controllers/UsersController.java
@@ -36,7 +36,10 @@ public class UsersController {
 
     @PostMapping("/")
     public Users add_user(@RequestBody Users u){
-
+//        if((u.getPhone_no().length()!=10)){
+//
+//        }
+//        if(u.getEmail().substring(u.getEmail().length()-))
         return usersRepository.save(u);
     }
 
